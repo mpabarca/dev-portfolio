08214cc6b625150de629cd2f61829f101babdfb7
mpabarca <maria@shape.dk>
2024-09-20 13:57:32 +0200

    Add translations, structure and styles to Blog Module

--- src/modules/Blog/index.tsx
+++ src/modules/Blog/index.tsx
@@ -1,8 +1,11 @@
 import { ISiteContext } from "@/interfaces";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export interface IBlogData {
-  title: string
-  intro: string
+  title: string;
+  mediumButton: string;
+  subtitle: string;
 }
 
 interface BlogModuleProps {
@@ -13,11 +16,30 @@ interface BlogModuleProps {
 
 const BlogModule = ({ id, context, content }: BlogModuleProps) => {
   return (
-    <section id={id} className="w-full h-screen p-8 flex flex-col items-start justify-center">
-      <h1>{content.title}</h1>
-      <p>{content.intro}</p>
-    </section>
-  )
-}
+    <section
+      id={id}
+      className='w-full h-screen container flex flex-col items-start justify-center gap-7'
+    >
+    <header className='flex flex-col gap-8'>
+      <h2 className='text-4xl lg:text-5xl font-semibold'>{content.title}</h2>
+      <nav>
+        <ul className='flex flex-col lg:flex-row gap-4'>
+          <li>
+          <Link
+            className={buttonVariants({ variant: "default" })}
+            href='#contact'
+          >
+            {content.mediumButton}
+          </Link>
+          </li>
+          {/* Link to Spotify Podcast Profile */}
+        </ul>
+      </nav>
+      <p className='text-lg lg:text-xl font-normal'>{content.subtitle}</p>
+    </header>
 
-export default BlogModule
+    </section>
+  );
+};
+
+export default BlogModule;
